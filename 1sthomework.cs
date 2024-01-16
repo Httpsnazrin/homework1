b9bbe7792fb6539cce587642cb6289a11219e83c
Httpsnazrin <147836567+Httpsnazrin@users.noreply.github.com>
2024-01-16 23:45:14 +0400

    Add files via upload

---
+++ 1sthomework.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region Task 1
+//string department=Console.ReadLine();
+//if (department == "HR")
+//{
+//    Console.WriteLine("İnsan Resursları sobesi");
+//}
+//else if (department == "IT")
+//{
+//    Console.WriteLine( "İnformasiya texnologiyalari sobesi");
+//}
+//else if (department== "Finance")
+//{
+//    Console.WriteLine("Finans sobesi");
+
+//}
+//else
+//{
+//    Console.WriteLine("Bele bir sobe yoxdur");
+//}
+#endregion
+#region Task 2
+using Microsoft.VisualBasic;
+using System.Reflection.Metadata.Ecma335;
+
+//int staj = Convert.ToInt32(Console.ReadLine());
+
+//if (staj > 0 && staj < 2)
+//{
+//    Console.WriteLine("Sizin maasiniz 2000");
+//}
+//else if (staj > 3 && staj <= 5)
+//{
+//    Console.WriteLine("Sizin maasiniz 5000");
+//}
+//else if (staj > 5 && staj <= 10)
+//{
+//    Console.WriteLine("Sizin maasiniz 7000");
+//}
+//else if (staj > 10)
+//{
+//    Console.WriteLine("Sizin maasiniz 10000");
+//}
+
+#endregion
+#region Task 3
+//int hours=Convert.ToInt32(Console.ReadLine());
+//if (hours>0 && hours<=12)
+//{
+//    Console.WriteLine("Sabahiniz xeyir.");
+//}
+//else if (hours>12 && hours<=17)
+//{
+//    Console.WriteLine("Gunortaniz xeyir.");
+//}
+//else if (hours>17 && hours <= 24)
+//{
+//    Console.WriteLine("Axsaminiz xeyir.");
+//}
+
+#endregion
+#region Task 4
+//int number1=Convert.ToInt32(Console.ReadLine());
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//int number3 = Convert.ToInt32(Console.ReadLine());
+//int avarage=(number1+ number2+number3)/3;
+//Console.WriteLine($"Ortalama:{avarage}");
+//if (avarage <= 20)
+//{
+//    Console.WriteLine("Aktiv");
+//}
+//else if (avarage >20)
+//{
+//    Console.WriteLine("Deaktiv");
+//}
+#endregion
